077de5a763a6805e988174a425eeca7cea065b39
eric <eric_schumertl@hotmail.com>
2020-10-14 16:16:42 +0200

    Add monaco-editor
    Simple implementation of monaco-editor

---
+++ src/components/MonacoEditor.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    id="app"
+    style="width: 1000px; height: 1000px;"
+  >
+    <div
+      ref="monacoEditor"
+      style="width: 1000px; height: 1000px;"
+    ></div>
+  </div>
+</template>
+
+<script>
+import * as monaco from "monaco-editor";
+
+export default {
+  name: "MonacoEditor",
+  async mounted() {
+    const el = this.$refs.monacoEditor;
+    this.editor = monaco.editor.create(el, {
+      value: "test",
+      language: "javascript"
+    });
+  }
+};
+</script>
